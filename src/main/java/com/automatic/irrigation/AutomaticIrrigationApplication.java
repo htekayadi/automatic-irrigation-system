1c3762f3adde7052deab9e781b6fd1ca1e2243e6
Hendrik Tekayadi <htekayadi@gmail.com>
2022-12-31 12:39:47 +0700

    Initial Commit

---
+++ src/main/java/com/automatic/irrigation/AutomaticIrrigationApplication.java
@@ -0,0 +1,14 @@
+package com.automatic.irrigation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutomaticIrrigationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutomaticIrrigationApplication.class, args);
+	}
+
+}
+
